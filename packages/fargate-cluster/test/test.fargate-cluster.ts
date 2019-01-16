957504be8ace36db6c5d969f1d7f00355afd6f21
Philipp Garbe <philipp.garbe@web.de>
2019-01-16 19:42:56 +0100

    Also, add the tests

---
+++ packages/fargate-cluster/test/test.fargate-cluster.ts
@@ -0,0 +1,24 @@
+import { expect, haveResource } from '@aws-cdk/assert';
+import ec2 = require('@aws-cdk/aws-ec2');
+import cdk = require('@aws-cdk/cdk');
+import { Test } from 'nodeunit';
+import fc = require('../lib');
+
+export = {
+  'test ECS loadbalanced construct'(test: Test) {
+    // GIVEN
+    const stack = new cdk.Stack();
+    const vpc = new ec2.VpcNetwork(stack, 'VPC');
+
+    // WHEN
+    new fc.FargateCluster(stack, 'Service', {
+      vpc
+    });
+
+    // THEN - stack containers a load balancer
+    expect(stack).to(haveResource('AWS::ElasticLoadBalancingV2::LoadBalancer', {
+    }));
+
+    test.done();
+  }
+};
